535ccf1310b8782ab96dc0c4571f5ed0a87be806
Md-Akil-056 <93811274+056AKIL@users.noreply.github.com>
2023-12-23 19:23:00 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12344;
+// accountId = 123123 change const value is not allowed !!
+let accountEmail = "akil@gmail.com";
+var accountPassword = "12345"; // ye Globally declear hota h or multiple times same name se create krne pr v kaam kr jata h
+accountCity = "Patna"; // Aese bhi variable create nhi krna chahiye
+let accountState; //Undefined return krega
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountCity,accountState,accountPassword])
