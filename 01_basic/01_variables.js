6111494f08af0aec839f987a5ef960e942b9e863
Md-Akil-056 <93811274+056AKIL@users.noreply.github.com>
2023-12-23 19:50:19 +0000

    container config done

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,7 +1,11 @@
 const accountId = 12344;
 // accountId = 123123 change const value is not allowed !!
 let accountEmail = "akil@gmail.com";
-var accountPassword = "12345"; // ye Globally declear hota h or multiple times same name se create krne pr v kaam kr jata h
+var accountPassword = "12345"; 
+/*
+Prefer not to use var
+Because of issues in Block scope and functional scope
+*/
 accountCity = "Patna"; // Aese bhi variable create nhi krna chahiye
 let accountState; //Undefined return krega
 
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JavaScript code as newer version
+
+let fullName = "Akil" 
+let age = 18      
+let isFollow = true 
+let rollNumber      
+let sex = null      // null
+
+// number => 2 to the power 53
+// string => ""
+// boolean => true / false
+// Bigint => 
+// Symbol for Uniqueness
+// Undefined =>
+// null => standalone value
+
+console.table([fullName, rollNumber, age, isFollow, sex])
