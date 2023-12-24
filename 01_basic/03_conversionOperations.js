a0abc4d7026402836dc062da608279a900f71981
Md-Akil-056 <93811274+056AKIL@users.noreply.github.com>
2023-12-24 02:15:39 +0000

    conversion & Operations

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -1,27 +1,68 @@
-// ***** CONVERSIONS *****
-let score = "33aa"
+let score = "hitesh"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
 
 // "33" => 33
-// "abc32" => NaN
-// true => 1 , false => 0
+// "33abc" => NaN
+//  true => 1; false => 0
 
-let isLoggedIn = ""
+let isLoggedIn = "hitesh"
+
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
 
-// "" => false , "akil" => true
-// 1 => true , 0 => false
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
 
-let someNumber = 35
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber) // string
+let someNumber = 33
 
-// ***** OPERATIONS *****
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
